6dd585ecb02fabb9a43f29301988efd18ae35048
indrapramana <idewebid@gmail.com>
2021-02-20 15:54:18 +0700

    add appointment module

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -14,34 +14,80 @@ class AppointmentController extends Controller
 		$appointments = Appointment::all();
 
 		if ($appointments->count() > 0) {
-
+			return AppointmentResource::collection($appointments);
 		} else {
-
+			return response()->json(['message' => 'Not Found'], 400);
 		}
 	}
 
 	public function store(Request $request)
 	{
-		//
+		$validator = Validator::make($request->all(), [
+			'clinic_id' => 'required',
+			'patient_id' => 'required',
+			'officer_id' => 'required',
+			'appointment_at' => 'required',
+		]);
+
+		if ($validator->fails()) {
+			return response()->json(['errors' => $validator->errors()], 400);
+		} else {
+			$appointment = new Appointment();
+			$appointment->clinic_id = $request->clinic_id;
+			$appointment->patient_id = $request->patient_id;
+			$appointment->officer_id = $request->officer_id;
+			$appointment->appointment_at = $request->appointment_at;
+			$appointment->save();
+
+			return response()->json(['success' => true, 'data' => $appointment], 200);
+		}
 	}
 
 	public function show($id)
 	{
-		//
-	}
-
-	public function edit($id)
-	{
-		//
+		$appointment = Appointment::find($id);
+		
+		if ($appointment) {
+			return new AppointmentResource($appointment);
+		} else {
+			return response()->json(['message' => 'Not Found'], 404);
+		}
 	}
 
 	public function update(Request $request, $id)
 	{
-		//
+		$validator = Validator::make($request->all(), [
+			'patient_id' => 'required',
+			'officer_id' => 'required',
+			'appointment_at' => 'required',
+		]);
+
+		if ($validator->fails()) {
+			return response()->json(['errors' => $validator->errors()], 400);
+		} else {
+			$appointment = Appointment::find($id);
+
+			if ($appointment) {
+				$appointment->patient_id = $request->patient_id;
+				$appointment->officer_id = $request->officer_id;
+				$appointment->appointment_at = $request->appointment_at;
+				$appointment->save();
+				return response()->json(['success' => true, 'data' => $appointment], 200);
+			} else {
+				return response()->json(['message' => 'Not Found'], 404);
+			}
+		}
 	}
 
 	public function destroy($id)
 	{
-		//
+		$appointment = Appointment::find($id);
+
+		if ($appointment) {
+			$appointment->delete();
+			return response()->json(['success' => true, 'data' => $appointment], 200);
+		} else {
+			return response()->json(['message' => 'Not Found'], 404);
+		}
 	}
 }
